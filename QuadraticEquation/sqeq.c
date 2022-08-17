083decae0f432f6ca401cc0bc6ded8b3cade6af5
Alex Taranov <taranov.sasha2004@gmail.com>
2022-08-17 20:46:29 +0300

    Overwhelming code refactoring

--- QuadraticEquation/sqeq.c
+++ QuadraticEquation/sqeq.c
@@ -1,63 +1,43 @@
 #include <stdio.h>
 #include <math.h>
 
-static const int MAXLINE = 1000;
 
-int getNumbers(char s[], int maxlength, float num[]) {
-    int j, i, f;
-    bool have_dot, negative;
-    have_dot = negative = false;
-    ++num;
-    for (i = 0; (j = getchar()) != EOF; i++) {
-        if (j >= '0' && j <= '9') {
-            if (have_dot) {
-                *num = *num + (float)(j - '0')/(pow(10, f));
-                f++;
-            } else {
-            *num = *num * 10 + j - '0';
-            }
-        } else if (j == ',' || j == '.') {
-            f = 1;
-            have_dot = true;
-        } else if (j == '-') {
-            negative = true;
-        } else if (j == '\n') {
-            if (negative) {
-                *num *= -1;
-            }
-            ++num;
-            negative = false;
-            have_dot = false;
-        }
+double coeff[3]; // a, b, c
+                 // 0, 1, 2
+
+void getCoefficient(int k) {
+
+    printf("Enter argument %c:\n\t", 'a' + k);
+    scanf("%lf", &coeff[k]);
+    k++;
+
+}
+
+void giveAnswer(double a, double b, double c) {
+
+    double D = b * b - 4 * a * c;
+
+    printf("  Answer:\n");
+
+    if (D > 0) {
+        double x1 = (-b + sqrt(D))/(2 * a);
+        double x2 = (-b - sqrt(D))/(2 * a);
+        printf("\tD = %lf,\n\tx1 = %lf,\n\tx2 = %lf", D, x1, x2);
+    } else if (D == 0) {
+        double x = (-b)/(2 * a);
+        printf("\tD = %lf,\n\tx1 = x2 = %lf", D, x);
+    } else {
+        printf("\tD = %lf,\n\tx1, x2 in complex field", D);
     }
-    return i;
 }
 
 int main() {
-    float num[MAXLINE]; // len, a, b, c, D, x1, x2;
-    char    s[MAXLINE]; //   0, 1, 2, 3, 4,  5,  6;
-    printf("Enter a, b, c of quadratic equation\nExample:\n\t115\n\t2\n\t5\n");
-    while (num[0] = getNumbers(s, MAXLINE, num)) {
-        ;
+
+    for (int i = 0; i < 3; i++) {
+        getCoefficient(i);
     }
 
-    float a = num[1];
-    float b = num[2];
-    float c = num[3];
-
-    printf("a = %f, b = %f, c = %f\n", a, b, c);
-    float D = b * b - 4 * a * c;
-
-    if (D > 0) {
-        float x1 = (-b + sqrt(D))/(2 * a);
-        float x2 = (-b - sqrt(D))/(2 * a);
-        printf("\tD = %f,\n\tx1 = %f,\n\tx2 = %f", D, x1, x2);
-    } else if (D == 0) {
-        float x = (-b)/(2 * a);
-        printf("\tD = %f,\n\tx1 = x2 = %f", D, x);
-    } else {
-        printf("\tD = %f,\n\tx1, x2 in complex field", D);
-    }
+    giveAnswer(coeff[0], coeff[1], coeff[2]);
 
     return 0;
 }
