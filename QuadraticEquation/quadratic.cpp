b4aa4083099fdd6cb136f9167e34114dc3e1ccc9
Alex Taranov <taranov.sasha2004@gmail.com>
2022-08-23 18:20:53 +0300

    [1] Refactor quadratic.cpp
    - Structure Equation --> array roots and struct polynomial

--- QuadraticEquation/quadratic.cpp
+++ QuadraticEquation/quadratic.cpp
@@ -1,75 +1,88 @@
 #include <stdio.h>
 #include <math.h>
 
+static const MAX_NUM_ROOTS = 2;
+static const SS_INF_ROOTS = -1;
 
-struct Equation {
-
-    double a = NAN;
-    double b = NAN;
-    double c = NAN;
-    double x1 = NAN;
-    double x2 = NAN;
-    int root_counter = 0;
-
+struct polynomial
+{
+        const double *coeffs = nullptr;
+        int degree = 0;
 };
+
 
 static const double epsilon = 1e-15;
 
-void get_coefficient(Equation *equ) {
 
-    printf("Enter 3 coefficients of quadratic equation:\n");
-    scanf("%lf %lf %lf", &equ->a, &equ->b, &equ->c);
-}
-
-void calculate_lin_ans(Equation *equ) {
-    equ->x1 = -(equ->c) / (equ->b);
-    equ->root_counter = 1;
-}
-
-void calculate_quadr_answer(Equation *equ) {
-    double D = equ->b * equ->b - 4 * equ->a * equ->c;
-    if (equ->a != 0) {
-        if (fabs(D) < epsilon) {
-            equ->x1 = (-equ->b)/(2 * equ->a);
-            equ->root_counter = 1;
-        } else if (D > 0) {
-            equ->x1 = (-equ->b + sqrt(D))/(2 * equ->a);
-            equ->x2 = (-equ->b - sqrt(D))/(2 * equ->a);
-            equ->root_counter = 2;
-        } else {
-        equ->root_counter = 0;
+void get_coefficients(Polynomial *pol)
+{
+        for (int i = 0; i < MAX_NUM_ROOTS + 1; i++) {
+                printf("Enter %d coefficient of equation:\n", i);
+                scanf("%lf\n", pol->coeffs[i]);
         }
-    } else if (equ->b == 0 && equ->c == 0) {
-        equ->root_counter = -1;
-    } else {
-        calculate_lin_ans(equ);
-    }
+//        int arr[2] = {0};
+//        const int *ptr = arr;
+//        int *const arr
+//        arr++;
 }
 
-void give_answer(Equation *equ) {
+void calculate_lin_ans(double roots[], Polynomial *pol)
+{
+        if (b == 0 && c == 0) {
+                pol->degree = SS_INF_ROOTS;
+        } else {
+                roots[0] = -(pol->coeffs[0]) / (pol->coeffs[1]);
+                pol->degree = 1;
+        }
+}
 
-    printf("  Answer:\n");
+void calculate_quadr_answer(double roots[], Polynomial *pol)
+{
+        double *coeffs = pol->coeffs;
+        double D = b * b - 4 * a * c;
+        if (a != 0) {
+                if (fabs(D) < epsilon) {
+                        roots[0] = (-coeffs[1]) / (2 * coeffs[0]);
+                        pol->degree = 1;
+                } else if (D > 0) {
+                        roots[0] = (-coeffs[1] + sqrt(D)) / (2 * coeffs[0]);
+                        roots[1] = (-b - sqrt(D)) / (2 * coeffs[0]);
+                        pol->degree = 2;
+                } else {
+                        pol->degree = 0;
+                }
+        } else {
+                calculate_lin_ans(roots, pol);
+        }
+}
 
-    if (equ->root_counter == 2) {
-        printf("\n\tx1 = %lf,\n\tx2 = %lf.\n", equ->x1, equ->x2);
-    } else if (equ->root_counter == 1) {
-        printf("\tx1 = %lf.\n", equ->x1);
-    } else if (equ->root_counter == -1) {
-        printf("\tThere infinite number of roots.\n");
-    } else {
-        printf("\tThere are no roots.\n");
-    }
-
+void give_answer(double roots[])
+{
+        printf("  Answer:\n");
+        switch (degree) {
+                case 2: printf("\n\tx1 = %lf,\n\tx2 = %lf.\n", roots[0], roots[1]);
+                        break;
+                case 1:
+                        printf("\tThere infinite number of roots.\n");
+                        break;
+                default: printf("\tThere are no roots.\n");
+        }
 }
 
 
-int main() {
+int main()
+{
+        double coeffs[MAX_NUM_ROOTS + 1] = {0};
+        double roots[MAX_NUM_ROOTS] = {0};
 
-    Equation equ = {};
+        Polynomial pol = {
+                .coeffs = coeffs,
+                .degree = degree
+        };
 
-    get_coefficient(&equ);
-    calculate_quadr_answer(&equ);
-    give_answer(&equ);
+        get_coefficient();
+        calculate_quadr_answer();
+        give_answer();
 
-    return 0;
+        return 0;
 }
