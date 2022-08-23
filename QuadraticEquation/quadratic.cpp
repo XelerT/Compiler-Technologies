15170fd6b52c7d0684fb19f6b2cc2380b278020e
Alex Taranov <taranov.sasha2004@gmail.com>
2022-08-23 20:41:23 +0300

    [2]

--- QuadraticEquation/quadratic.cpp
+++ QuadraticEquation/quadratic.cpp
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 
-static const MAX_NUM_ROOTS = 2;
-static const SS_INF_ROOTS = -1;
+static const int MAX_NUM_ROOTS = 2;
+static const int SS_INF_ROOTS = -1;
 
 struct polynomial
 {
@@ -10,25 +10,20 @@ struct polynomial
         int degree = 0;
 };
 
-
 static const double epsilon = 1e-15;
 
 
-void get_coefficients(Polynomial *pol)
+void get_coefficients(polynomial *pol)
 {
         for (int i = 0; i < MAX_NUM_ROOTS + 1; i++) {
-                printf("Enter %d coefficient of equation:\n", i);
-                scanf("%lf\n", pol->coeffs[i]);
+                printf("Enter %d coefficient of equation:\n", i + 1);
+                scanf("%lf\n", &pol->coeffs[i]);
         }
-//        int arr[2] = {0};
-//        const int *ptr = arr;
-//        int *const arr
-//        arr++;
 }
 
-void calculate_lin_ans(double roots[], Polynomial *pol)
+void calculate_lin_ans(double *roots, polynomial *pol)
 {
-        if (b == 0 && c == 0) {
+        if (pol->coeffs[1] == 0 && pol->coeffs[2] == 0) {
                 pol->degree = SS_INF_ROOTS;
         } else {
                 roots[0] = -(pol->coeffs[0]) / (pol->coeffs[1]);
@@ -36,17 +31,16 @@ void calculate_lin_ans(double roots[], Polynomial *pol)
         }
 }
 
-void calculate_quadr_answer(double roots[], Polynomial *pol)
+void calculate_quadr_answer(double *roots, const polynomial *pol)
 {
-        double *coeffs = pol->coeffs;
-        double D = b * b - 4 * a * c;
-        if (a != 0) {
-                if (fabs(D) < epsilon) {
-                        roots[0] = (-coeffs[1]) / (2 * coeffs[0]);
+        double Discr = pol->coeffs[1] * pol->coeffs[1] - 4 * pol->coeffs[0] * pol->coeffs[2];
+        if (pol->coeffs[0] != 0) {
+                if (fabs(Discr) < epsilon) {
+                        roots[0] = (-pol->coeffs[1]) / (2 * pol->coeffs[0]);
                         pol->degree = 1;
-                } else if (D > 0) {
-                        roots[0] = (-coeffs[1] + sqrt(D)) / (2 * coeffs[0]);
-                        roots[1] = (-b - sqrt(D)) / (2 * coeffs[0]);
+                } else if (Discr > 0) {
+                        roots[0] = (-pol->coeffs[1] + sqrt(Discr)) / (2 * pol->coeffs[0]);
+                        roots[1] = (-pol->coeffs[1] - sqrt(Discr)) / (2 * pol->coeffs[0]);
                         pol->degree = 2;
                 } else {
                         pol->degree = 0;
@@ -56,15 +50,16 @@ void calculate_quadr_answer(double roots[], Polynomial *pol)
         }
 }
 
-void give_answer(double roots[])
+void give_answer(double roots[], polynomial *pol)
 {
         printf("  Answer:\n");
-        switch (degree) {
+        switch (pol->degree) {
                 case 2: printf("\n\tx1 = %lf,\n\tx2 = %lf.\n", roots[0], roots[1]);
                         break;
-                case 1:
-                        printf("\tThere infinite number of roots.\n");
+                case 1: printf("\tx1 = %lf.\n", roots[0]);
                         break;
+                case SS_INF_ROOTS:  printf("\tThere infinite number of roots.\n");
+                                    break;
                 default: printf("\tThere are no roots.\n");
         }
 }
@@ -74,15 +69,19 @@ int main()
 {
         double coeffs[MAX_NUM_ROOTS + 1] = {0};
         double roots[MAX_NUM_ROOTS] = {0};
+        int degree = 0;
 
-        Polynomial pol = {
-                .coeffs = coeffs,
-                .degree = degree
-        };
+//        polynomial pol = {
+//                .coeffs = coeffs,
+//                .degree = degree
+//        };
+        polynomial pol = {};
+        pol.coeffs = coeffs;
+        pol.degree = degree;
 
-        get_coefficient();
-        calculate_quadr_answer();
-        give_answer();
+        get_coefficients(&pol);
+        //calculate_quadr_answer(roots, &pol);
+        //give_answer(roots, &pol);
 
         return 0;
 }
