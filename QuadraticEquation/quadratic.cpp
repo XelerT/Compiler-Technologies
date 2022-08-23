a219374693e468a453abf7fa007197154185de7a
Alex Taranov <taranov.sasha2004@gmail.com>
2022-08-23 21:51:34 +0300

    Edits tabs, enters and pointers

--- QuadraticEquation/quadratic.cpp
+++ QuadraticEquation/quadratic.cpp
@@ -13,11 +13,11 @@ struct polynomial
 static const double epsilon = 1e-15;
 
 
-void get_coefficients(polynomial *pol)
+void get_coefficients(polynomial *poly)
 {
         for (int i = 0; i < MAX_NUM_ROOTS + 1; i++) {
                 printf("Enter %d coefficient of equation:\n", i + 1);
-                scanf(" %lf", &pol->coeffs[i]);
+                scanf(" %lf", &poly->coeffs[i]);
         }
 }
 
@@ -25,11 +25,9 @@ int solve_linear(double *roots, const polynomial *poly)
 {
         const double *coeffs = poly->coeffs;
         if (coeffs[1] == 0 && coeffs[2] == 0) {
-
                 return SS_INF_ROOTS;
         } else {
                 roots[0] = -coeffs[0] / coeffs[1];
-
                 return 1;
         }
 }
@@ -41,18 +39,16 @@ int solve_quadratic(double *roots, const polynomial *poly)
         if (coeffs[0] != 0) {
                 if (fabs(discr) < epsilon) {
                         roots[0] = (-coeffs[1]) / (2 * coeffs[0]);
-
                         return 1;
                 } else if (discr > 0) {
                         roots[0] = (-coeffs[1] + sqrt(discr)) / (2 * coeffs[0]);
                         roots[1] = (-coeffs[1] - sqrt(discr)) / (2 * coeffs[0]);
-
                         return 2;
                 } else {
                         return 0;
                 }
         } else {
-                return calculate_lin_ans(roots, poly);
+                return solve_linear(roots, poly);
         }
 }
 
@@ -60,13 +56,18 @@ void give_answer(double *roots, int n_roots)
 {
         printf("  Answer:\n");
         switch (n_roots) {
-                case 2: printf("\n\tx1 = %lf,\n\tx2 = %lf.\n", roots[0], roots[1]);
-                        break;
-                case 1: printf("\tx1 = %lf.\n", roots[0]);
-                        break;
-                case SS_INF_ROOTS:  printf("\tThere infinite number of roots.\n");
-                                    break;
-                default: printf("\tThere are no roots.\n");
+        case 2:
+                printf("\n\tx1 = %lf,\n\tx2 = %lf.\n", roots[0], roots[1]);
+                break;
+        case 1:
+                printf("\tx1 = %lf.\n", roots[0]);
+                break;
+        case SS_INF_ROOTS:
+                printf("\tThere infinite number of roots.\n");
+                break;
+        default:
+                printf("\tThere are no roots.\n");
+                break;
         }
 }
 
@@ -87,7 +88,7 @@ int main()
         poly.degree = degree;
 
         get_coefficients(&poly);
-        n_roots = calculate_quadr_answer(roots, &poly);
+        n_roots = solve_quadratic(roots, &poly);
         give_answer(roots, n_roots);
 
         return 0;
