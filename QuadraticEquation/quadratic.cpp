2d16088b450e6d85b40d4a30935e7b7822472e4d
Alex Taranov <taranov.sasha2004@gmail.com>
2022-08-24 07:44:11 +0300

    [6] Added degree request
    -Need to create solution with dinamic(or smth else) arrays.

--- QuadraticEquation/quadratic.cpp
+++ QuadraticEquation/quadratic.cpp
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <math.h>
 
-static const int MAX_NUM_ROOTS = 2;
-static const int SS_INF_ROOTS = -1;
+enum  {
+     NO_ROOTS =  0,
+    INF_ROOTS = -1,
+    POSSIBLE_EQU_DEGREE = 2, //N_MAX_DEGREE
+
+};
 
 struct polynomial
 {
@@ -15,7 +19,7 @@ static const double epsilon = 1e-15;
 
 void get_coefficients(polynomial *poly)
 {
-        for (int i = 0; i < MAX_NUM_ROOTS + 1; i++) {
+        for (int i = 0; i < poly->degree + 1; i++) {
                 printf("Enter %d coefficient of equation:\n", i + 1);
                 scanf(" %lf", &poly->coeffs[i]);
         }
@@ -25,7 +29,7 @@ int solve_linear(double *roots, const polynomial *poly)
 {
         const double *coeffs = poly->coeffs;
         if (coeffs[1] == 0 && coeffs[2] == 0) {
-                return SS_INF_ROOTS;
+                return INF_ROOTS;
         } else {
                 roots[0] = -coeffs[0] / coeffs[1];
                 return 1;
@@ -56,27 +60,55 @@ void give_answer(double *roots, int n_roots)
 {
         printf("  Answer:\n");
         switch (n_roots) {
-        case 2:
-                printf("\n\tx1 = %lf,\n\tx2 = %lf.\n", roots[0], roots[1]);
-                break;
-        case 1:
-                printf("\tx1 = %lf.\n", roots[0]);
-                break;
-        case SS_INF_ROOTS:
+
+        case INF_ROOTS:
                 printf("\tThere infinite number of roots.\n");
                 break;
-        default:
+        case NO_ROOTS:
                 printf("\tThere are no roots.\n");
                 break;
         }
+
+        for (int i = 0; i < n_roots; i++) {
+                printf("\tx%d = %lf\n", i + 1, roots[i]);
+        }
 }
+
+int get_degree(void) {
+        int degree = 0;
+        printf("Enter degree of your equation, please.\n");
+        while (degree == 0) {
+                scanf("%d", &degree);
+                if (degree > POSSIBLE_EQU_DEGREE) {
+                        degree = 0;
+                        printf("Sorry, we can't solve your equation(degree is too high).\n Enter degree of your equation again.\n");
+                } else if (degree <= 0) {
+                        degree = 0;
+                        printf("Sorry, we can't solve your equation(degree is too low).\n Enter degree of your equation again.\n ");
+                }
+        }
+        return degree;
+}
+
+//int solve_cubic (double *roots, const polynomial *poly) {
+//        const double *coeffs = polly->coeffs;
+//}
 
 
 int main()
 {
-        double coeffs[MAX_NUM_ROOTS + 1] = {0};
-        double roots[MAX_NUM_ROOTS] = {0};
-        int degree = 0;
+        int degree = get_degree();
+//        if (degree == 1) {
+//                double coeffs[2] = {0};
+//                double roots[1] = {0};
+//        } else if (degree == 2) {
+//                double coeffs[3] = {0};
+//                double roots[2] = {0};
+//        }
+
+        double coeffs[2 + 1] = {0};
+        double roots[2] = {0};
+
         int n_roots = 0;
 
 //        polynomial pol = {
