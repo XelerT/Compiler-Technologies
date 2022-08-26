2cf50b65090612800b2be7a555464475cf7a1afc
Alex Taranov <taranov.sasha2004@gmail.com>
2022-08-26 17:02:04 +0300

    Added unitests

--- QuadraticEquation/unitests.cpp
+++ QuadraticEquation/unitests.cpp
@@ -1,11 +1,94 @@
 #include <stdio.h>
+#include <assert.h>
+#include "ui.h"
 
-struct unitest {
+static const int N_TESTS = 1;
+static const int MAX_DEGREE = 2;
 
+struct test_t {
+
+        polynomial_t poly = {0};
+        const double *roots  = nullptr;
 };
 
+void check_test(const test_t *test, double *roots, const double right_roots[], int i, int n_roots, int right_n_roots);
+int compare_array(const double *arr1, double *arr2, const double threshold);
+void run_test(double a, double b, double c, double d, double e, int right_n_roots);
+void pr_true();
+
+void check_test(const test_t *test, double *roots, const double *right_roots, int i, int n_roots, int right_n_roots)
+{
+        if (n_roots == INF_ROOTS) {
+                pr_true();
+        } else if (n_roots == WTF) {
+                pr_true();
+        } else if (n_roots == NO_ROOTS) {
+                pr_true();
+        } else {
+                if (compare_array(right_roots, roots, THRESHOLD)) {
+                        pr_true();
+                } else {
+                        printf("Test %d:\t***FALSE***.\n", i);
+                }
+        }
+}
+
+void pr_true()
+{
+        printf("Test:\t TRUE.\n");
+}
+
+int compare_array(const double *arr1, double *arr2, const double threshold)
+{
+        for (int i = 0; i < MAX_DEGREE; i++) {
+                if (!is_equal(arr1[i], arr2[i], threshold))
+                        return 0;
+        }
+        return true;
+}
+
+void run_test(double a, double b, double c, double d, double e, int right_n_roots) {
+
+        double roots[MAX_DEGREE] = {0};
+        double coeffs[MAX_DEGREE] = {0};
+        double right_roots[MAX_DEGREE] = {0};
+        int n_roots = 0;
+
+        coeffs[0] = a;
+        coeffs[1] = b;
+        coeffs[2] = c;
+
+        right_roots[0] = d;
+        right_roots[1] = e;
+
+        test_t test[N_TESTS] = {0};
+
+        test[0] = {
+
+                {
+                         coeffs,
+                         2
+                 },
+                 right_roots,
+
+        };
+
+        for (int i = 0; i < N_TESTS; i++) {
+                n_roots = solve_polynomial(roots, &(test[i].poly));
+                check_test(&test[i], roots, test[i].roots, i, n_roots, right_n_roots);
+        }
+
+}
 
 int main()
 {
+        run_test(2, -8.5, 8, 2.84307, 1.40692, 2);
+        run_test(2, -8, 8, 2, 0, 1);
+        run_test(5, -25, 8, 4.65638, 0.34361, 2);
+        run_test(134, 123, 5, -0.04263, -0.87528, 2);
+        run_test(0, 0, 0, 0, 0, INF_ROOTS);
+        run_test(1, 1, 1, 0, 0, NO_ROOTS);
+        run_test(0, 0, 1, 0, 0, WTF);
 
+        return 0;
 }
